8fe349e214e7ab13ed6bfcfa390cfed5926a4898
Nhut Phung <nhutmphung@gmail.com>
2025-10-18 21:24:35 -0500

    completed draw feature

--- ttt.cpp
+++ ttt.cpp
@@ -56,23 +56,26 @@ class Board {
             return board[row][column];
         }
 
-        void checkWin(void) {
+        bool checkWin() {
             
             for(int i = 0; i < 3; i++){     //check if won via x rows
                 if(board[i][0] == 'x' &&  board[i][1] == 'x' && board[i][2] == 'x'){
                     std::cout << "Congrats, you won!";
+                    return true;
                 }
             }
             
             for(int i = 0; i < 3; i++){     //check if won via o rows
                 if(board[i][0] == 'o' && board[i][1] == 'o' && board[i][2] == 'o'){
                     std::cout << "Congrats, you won!";
+                    return true;
                 }
             }
             
             for(int i = 0; i < 3; i++){     //check if won via x columns
                 if(board[0][i] == 'x' &&  board[1][i] == 'x' && board[2][i] == 'x'){
                     std::cout << "Congrats, you won!";
+                    return true;
                 }
             }
 
@@ -80,29 +83,47 @@ class Board {
             for(int i = 0; i < 3; i++){     //check if won via o columns
                 if(board[0][i] == 'o' && board[1][i] == 'o' && board[2][i] == 'o'){
                     std::cout << "Congrats, you won!";
+                    return true;
                 }
             }
 
             if (board[0][0] == 'x' && board[1][1] == 'x' && board[2][2] == 'x'){ //checking for win left to right diag for x
                 std::cout << "Congrats, you won!";
+                return true;
             }
             if (board[0][0] == 'o' && board[1][1] == 'o' && board[2][2] == 'o'){ //checking for win left to right diag for o
                 std::cout << "Congrats, you won!";
+                return true;
             }
             if (board[0][2] == 'x' && board[1][1] == 'x' && board[2][0] == 'x'){ //checking for win right to left diag for x
                 std::cout << "Congrats, you won!";
+                return true;
             }
-
             if (board[0][2] == 'o' && board[1][1] == 'o' && board[2][0] == 'o'){//cheecking for win right to left diag for o
                 std::cout << "Congrats, you won!";
+                return true;
             }
+
+            return false;
             
         }
 
         //*TODO if the checkWin is FALSE and all cells are fill, print out draw
-        void checkDraw(void) {
-            //Check if checkWIn is FALSE and vector is filled, print out draw
+        bool checkDraw() {
+            if (checkWin() == true){
+                return false; 
+            }
 
+            for (int i = 0; i < 3; i++){
+                for (int j = 0; j < 3; j++){
+                    if (board[i][j] != ' '){
+                        std::cout << "It was a draw.";
+                        return true;
+                    }
+                }
+            }
+            
+            
         }
 
 
@@ -125,10 +146,21 @@ int main(void)
 
 
     Board.placeMark(0, 0, 'x');
-    Board.placeMark(1, 1, 'x');
+    Board.placeMark(1, 0, 'o');
     Board.placeMark(2, 0, 'x');
 
+    Board.placeMark(0, 1, 'o');
+    Board.placeMark(1, 1, 'x');
+    Board.placeMark(2, 1, 'x');
+
+    Board.placeMark(0, 2, 'o');
+    Board.placeMark(1, 2, 'x');
+    Board.placeMark(2, 2, 'o');
+
+
+
     Board.checkWin();
+    Board.checkDraw();
 
 
     return 0;
